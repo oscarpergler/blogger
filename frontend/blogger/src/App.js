0b6d1cfda32397dcc6de8c35a4f5df33ea97fc83
b21oscpe <oscarpergler@gmail.com>
2024-01-29 15:53:05 +0100

    frontend init, changed file-structure

---
+++ frontend/blogger/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <h1>Users</h1>
+      <h1>Posts</h1>
+    </div>
+  );
+}
+
+export default App;
