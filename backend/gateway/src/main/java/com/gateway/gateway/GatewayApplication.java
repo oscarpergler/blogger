a296eab1164028a515f08cc19b5ded572211a53e
b21oscpe <oscarpergler@gmail.com>
2024-02-05 15:59:43 +0100

    added gateway and service registry

--- backend/gateway/src/main/java/com/gateway/gateway/GatewayApplication.java
+++ backend/gateway/src/main/java/com/gateway/gateway/GatewayApplication.java
@@ -1,13 +1,32 @@
 package com.gateway.gateway;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
-
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(r -> r
+						.path("/user/**")
+						.filters(f -> f.stripPrefix(1))
+						.uri("lb://USER-SERVICE")) // user-service: port 8082
+				.route(p -> p
+						.path("/post/**")
+						.filters(f -> f.stripPrefix(1))
+						.uri("lb://POSTS-SERVICE")) // posts-service: port 8083
+				.build();
+	}
 }
--- backend/user-service/src/main/java/com/service/user/UsersRepository.java
+++ backend/user-service/src/main/java/com/service/user/UsersRepository.java
@@ -2,5 +2,4 @@ package com.service.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-@CrossOrigin
 interface UsersRepository extends JpaRepository<Users, Long> {}
